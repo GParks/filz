444d8d8720838f64dc0a8435eb7a55909b0cda98
Greg Parks <greg_a_parks@optum.com>
2022-09-28 11:36:22 -0500

    Wed. morning - addl updates; mostly paths to skip

--- test2.sh
+++ test2.sh
@@ -3,4 +3,4 @@
 CNT=200000
 ant compile
 time java -classpath /Users/gparks1/workspace/eclipse/filz/jackson-core-2.13.4.jar:/Users/gparks1/workspace/eclipse/filz/jackson-databind-2.13.4.jar:/Users/gparks1/workspace/eclipse/filz/jackson-annotations-2.13.4.jar:/Users/gparks1/workspace/eclipse/filz/target \
-    filz.Directory $CNT y > ant_run_$CNT_`date +%Y-%m-%d_%H%M`.txt 2>&1
+    filz.Directory $CNT y > ant_run_${CNT}_`date +%Y-%m-%d_%H%M`.txt 2>&1
